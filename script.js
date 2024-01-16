9333a2d0bac520b7df1f0023750eb9d9b6fadfba
abhishekkjainn <jainabhishek1904@gmail.com>
2024-01-17 00:43:38 +0530

    Page2 Basic Layout Design Completed

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let headercounter = 0;
+let tripbool = false;
 function headerMobile() {
   if (headercounter % 2 == 0) {
     let headerdiv = document.getElementById('headerlinksdivmobile');
@@ -10,5 +11,18 @@ function headerMobile() {
     headerdiv.style.transform = 'translateY(-65%)';
     headerdiv.style.transition = 'all 0.5s';
     headercounter = headercounter + 1;
+  }
+}
+
+function TripDecider() {
+  let onewaydiv = document.getElementById('oneway');
+  let rounddiv = document.getElementById('round');
+  tripbool = !tripbool;
+  if (tripbool) {
+    onewaydiv.style.backgroundColor = 'white';
+    rounddiv.style.backgroundColor = 'transparent';
+  } else {
+    onewaydiv.style.backgroundColor = 'transparent';
+    rounddiv.style.backgroundColor = 'white';
   }
 }
